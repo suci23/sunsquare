33c6347e13fc6642d68d8af80eade9c1098f5424
zaka.zaidan <zaka.smansa@gmail.com>
2013-07-13 22:41:29 +0700

    pindah rumah

---
+++ js/java.js
@@ -0,0 +1,13 @@
+function tentanghover(){
+	document.getElementById("tentang").innerHTML = "<a href='#sec2'><img src='images/menu_1_hover.png'></a>";
+}
+function tentangout(){
+	document.getElementById("tentang").innerHTML = "<a href='#sec2'><img src='images/menu_1_about.png'></a>";
+}
+function ngapainhover(){
+	document.getElementById("ngapain").innerHTML = "<a href='#sec3'><img src='images/menu_2_hover.png'></a>";
+}
+function ngapainout(){
+	document.getElementById("ngapain").innerHTML = "<a href='#sec3'><img src='images/menu_2_portofolio.png'></a>";
+}
+
